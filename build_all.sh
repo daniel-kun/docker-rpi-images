a11c983824ab6b57ca4859461d107422826e04d2
Daniel Albuschat <d.albuschat@gmail.com>
2018-01-31 13:12:54 +0000

    Initial commit

---
+++ build_all.sh
@@ -0,0 +1,5 @@
+docker build -t rpi-raspbian-base rpi-raspbian-base && \
+docker build -t rpi-raspbian-llvm rpi-raspbian-llvm && \
+docker build -t rpi-raspbian-ghc rpi-raspbian-ghc && \
+docker build -t rpi-raspbian-cabal rpi-raspbian-cabal
+
