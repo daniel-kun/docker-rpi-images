cd9cdba4484d8f85507e2b2bc738e0696fac769a
Daniel Albuschat <d.albuschat@gmail.com>
2018-02-01 21:45:11 +0000

    First draft, should basically work

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,6 @@
 docker build -t rpi-raspbian-base rpi-raspbian-base && \
 docker build -t rpi-raspbian-llvm rpi-raspbian-llvm && \
 docker build -t rpi-raspbian-ghc rpi-raspbian-ghc && \
-docker build -t rpi-raspbian-cabal rpi-raspbian-cabal
+docker build -t rpi-raspbian-cabal rpi-raspbian-cabal && \
+docker build -t rpi-raspbian-elm rpi-raspbian-elm
 
